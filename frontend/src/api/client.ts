3f746047be2f2cf7a14573ccc10ae01407fb05b0
jestelf_ <shirshov_a@inbox.ru>
2025-07-01 11:29:06 +0300

    Delete OLD

--- frontend/src/api/client.ts
+++
@@ -1,9 +0,0 @@
-// src/api/client.ts
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-});
-
-export const setToken = (token: string) =>
-  (api.defaults.headers.common["Authorization"] = `Bearer ${token}`);
