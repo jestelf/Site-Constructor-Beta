206b627ba1ca9a835955a12827af18e230e40b0c
Александр <41776352+jestelf@users.noreply.github.com>
2025-07-04 15:54:10 +0300

    Revert "Fix builder initialization after modularization"

--- ExPlast/sitebuilder/frontend/builder.js
+++ ExPlast/sitebuilder/frontend/builder.js
@@ -6,12 +6,10 @@ import Builder from './modules/builder-class.js';
 const builder = new Builder();
 window.builder = builder;
 
-// Инициализируем модули только после полной загрузки DOM
-window.addEventListener('DOMContentLoaded', () => {
-  initDrag(builder);
-  initToolbar(builder);
-  builder.init();
-});
+initDrag(builder);
+initToolbar(builder);
+
+builder.init();
 
 window.addBlock = (type, x = 20, y = 20) => addBlock(builder, type, x, y);
 export default builder;
